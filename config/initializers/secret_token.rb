32e4edf0f63564555442325e19266d2873bb87a1
Will Hastings <will@kalamuna.com>
2013-12-01 14:44:07 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebDevBookmarks::Application.config.secret_key_base = 'f46fe9e5ccd6fd5522c25cd7db5cb22785643d4f8cce47c370f2ffa00b44eeee6e0cd6b6a6540d2bd871a60264095636699b75520b1a0bb0ef70996215c9c6c3'
