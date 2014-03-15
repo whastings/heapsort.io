a5e985c4a64bb8f9d7c98baa1dc2cd53be9b29de
Will Hastings <will@objectdotcreate.net>
2014-03-15 11:11:28 -0700

    Add OpenShift secret key generator.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,7 +10,7 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-require 'securerandom'
+require File.join(Rails.root,'lib','openshift_secret_generator.rb')
 
 def secure_token
   token_file = Rails.root.join('.secret')
@@ -25,4 +25,7 @@ def secure_token
   end
 end
 
-WebDevBookmarks::Application.config.secret_key_base = secure_token
+WebDevBookmarks::Application.config.secret_key_base = initialize_secret(
+  :token,
+  '335a4e365ef2daeea969640d74e18f0e3cd9fae1abd8f4125691a880774ea6d456a29c0831aa6921bf86a710fe555e916f0673f5657619ec9df22e0409bec345'
+)
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-WebDevBookmarks::Application.config.session_store :cookie_store, key: '_web_dev_bookmarks_session'
+require File.join(Rails.root,'lib','openshift_secret_generator.rb')
+
+WebDevBookmarks::Application.config.session_store :cookie_store, key: initialize_secret(
+  :session_store,
+  '_railsapp_session'
+)
