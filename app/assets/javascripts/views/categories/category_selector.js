19fc0a1671fc31225330dd954dc0b641a18e01e9
Will Hastings <will@objectdotcreate.net>
2014-03-26 14:33:18 -0700

    Fix category selector's link highlighting and horizontal scrolling.

--- app/assets/javascripts/views/categories/category_selector.js
+++ app/assets/javascripts/views/categories/category_selector.js
@@ -4,7 +4,7 @@ var Categories = require('../../collections/categories'),
     CategoryDictionary = require('../../support/category_dictionary');
 
 var CategorySelector = module.exports = Backbone.View.extend({
-  className: 'category-selector clearfix',
+  className: 'category-selector-container clearfix',
   events: {
     'click .category-link': 'changeCategory'
   },
@@ -13,6 +13,7 @@ var CategorySelector = module.exports = Backbone.View.extend({
   changeCategory: function(event) {
     event.preventDefault();
     var $newCategoryLink = $(event.target);
+    $newCategoryLink.parents('ul').find('a').removeClass('selected');
     $newCategoryLink.addClass('selected');
     switchCategories.call(this, $newCategoryLink);
     this.render();
@@ -24,30 +25,35 @@ var CategorySelector = module.exports = Backbone.View.extend({
     this.listenTo(this.dictionary, 'reset', this.render);
     this.collection.fetch();
     this.currentCategory = 0;
-    this.renderedCategories = [];
+    this.columnCount = 0;
   },
 
   render: function() {
     var children = this.dictionary.getChildren(this.currentCategory);
-    if (!children || _.contains(this.renderedCategories, this.currentCategory)) {
+    if (!children || (this.$column && this.$column.categoryId === this.currentCategory)) {
       return this;
     }
     var $newColumn = $(this.template({children: toPojoArray(children)}));
     $newColumn.$parentColumn = this.$column;
     $newColumn.categoryId = this.currentCategory;
     this.$column = $newColumn;
+
+    this.columnCount += 1;
+    this.$el.width(this.columnCount * 325);
+
     this.$el.append(this.$column);
-    this.renderedCategories.push(this.currentCategory);
     return this;
   }
 });
 
 var removeColumns = function($column, targetId) {
   if (!$column.$parentColumn || $column.categoryId === targetId) {
+    this.$column = $column;
     return;
   }
   $column.remove();
-  removeColumns($column.$parentColumn, targetId);
+  this.columnCount -= 1;
+  removeColumns.call(this, $column.$parentColumn, targetId);
 };
 
 // If new category isn't a child of current one, remove current.
